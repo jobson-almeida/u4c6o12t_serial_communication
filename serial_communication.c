0365601cc5815e4de1fd9ac8e2ff135b2485fe9d
Jobson Almeida <jobson.ra@outlook.com>
2025-02-08 23:55:46 -0300

    chore: criar setup da comunicação i2c

--- serial_communication.c
+++ serial_communication.c
@@ -7,37 +7,42 @@
 #define I2C_PORT i2c1
 #define I2C_SDA 14
 #define I2C_SCL 15
-#define endereco 0x3C
+#define IC2_ADDRESS 0x3C
+
+ssd1306_t ssd; // variável da estrutura do display
+
+void setup_i2c()
+{
+    // initializa a I2C usando 400Khz.
+    i2c_init(I2C_PORT, 400 * 1000);
+
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);                       // define o SDA utilizando a função de GPIO para I2C
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);                       // define o SCL utilizando a função de GPIO para I2C
+    gpio_pull_up(I2C_SDA);                                           // define o pull up da linha de dados
+    gpio_pull_up(I2C_SCL);                                           // define o pull up na linha do clock
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, IC2_ADDRESS, I2C_PORT); // Inicializa o display
+    ssd1306_config(&ssd);                                            // configura o display
+    ssd1306_send_data(&ssd);                                         // envia os dados para o display
+}
 
 int main()
 {
-    // I2C Initialisation. Using it at 400Khz.
-    i2c_init(I2C_PORT, 400 * 1000);
-
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
-    gpio_pull_up(I2C_SDA);                                        // Pull up the data line
-    gpio_pull_up(I2C_SCL);                                        // Pull up the clock line
-    ssd1306_t ssd;                                                // Inicializa a estrutura do display
-    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
-    ssd1306_config(&ssd);                                         // Configura o display
-    ssd1306_send_data(&ssd);                                      // Envia os dados para o display
 
     // Limpa o display. O display inicia com todos os pixels apagados.
-    ssd1306_fill(&ssd, false);
-    ssd1306_send_data(&ssd);
+    // ssd1306_fill(&ssd, false);
+    // ssd1306_send_data(&ssd);
 
-    bool cor = true;
+    // bool cor = true;
     while (true)
     {
-        cor = !cor;
+        // cor = !cor;
         // Atualiza o conteúdo do display com animações
-        ssd1306_fill(&ssd, !cor);                           // Limpa o display
-        ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor);       // Desenha um retângulo
-        ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 10); // Desenha uma string
-        ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 30);   // Desenha uma string
-        ssd1306_draw_string(&ssd, "PROF WILTON", 15, 48);   // Desenha uma string
-        ssd1306_send_data(&ssd);                            // Atualiza o display
+        // ssd1306_fill(&ssd, !cor);                           // Limpa o display
+        // ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor);       // Desenha um retângulo
+        // ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 10); // Desenha uma string
+        // ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 30);   // Desenha uma string
+        // ssd1306_draw_string(&ssd, "PROF WILTON", 15, 48);   // Desenha uma string
+        // ssd1306_send_data(&ssd);                            // Atualiza o display
 
         sleep_ms(1000);
     }
